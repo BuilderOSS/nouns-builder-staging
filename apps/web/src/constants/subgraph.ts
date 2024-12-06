8cec98a9368b510901d93ef0b8ab01c5193a6541
0xMillz <37815163+0xMillz@users.noreply.github.com>
2024-12-05 21:06:32 -0600

    Deploy and update final subgraph URLs

--- apps/web/src/constants/subgraph.ts
+++ apps/web/src/constants/subgraph.ts
@@ -3,21 +3,21 @@ import { CHAIN_ID } from 'src/typings'
 // TODO: update all when all are deployed to the new BuilderDAO Goldsky account
 export const PUBLIC_SUBGRAPH_URL = {
   [CHAIN_ID.ETHEREUM]:
-    'https://api.goldsky.com/api/public/project_clkk1ucdyf6ak38svcatie9tf/subgraphs/nouns-builder-ethereum-mainnet/stable/gn',
+    'https://api.goldsky.com/api/public/project_cm33ek8kjx6pz010i2c3w8z25/subgraphs/nouns-builder-ethereum-mainnet/latest/gn',
   [CHAIN_ID.OPTIMISM]:
     'https://api.goldsky.com/api/public/project_cm33ek8kjx6pz010i2c3w8z25/subgraphs/nouns-builder-optimism-mainnet/latest/gn',
   [CHAIN_ID.SEPOLIA]:
     'https://api.goldsky.com/api/public/project_cm33ek8kjx6pz010i2c3w8z25/subgraphs/nouns-builder-ethereum-sepolia/latest/gn',
-  [CHAIN_ID.OPTIMISM_SEPOLIA]:
-    'https://api.goldsky.com/api/public/project_clkk1ucdyf6ak38svcatie9tf/subgraphs/nouns-builder-optimism-sepolia/stable/gn',
   [CHAIN_ID.BASE]:
-    'https://api.goldsky.com/api/public/project_cm33ek8kjx6pz010i2c3w8z25/subgraphs/nouns-builder-base-mainnet/latest/gn',
-  [CHAIN_ID.BASE_SEPOLIA]:
-    'https://api.goldsky.com/api/public/project_clkk1ucdyf6ak38svcatie9tf/subgraphs/nouns-builder-base-sepolia/stable/gn',
+      'https://api.goldsky.com/api/public/project_cm33ek8kjx6pz010i2c3w8z25/subgraphs/nouns-builder-base-mainnet/latest/gn',
   [CHAIN_ID.ZORA]:
-    'https://api.goldsky.com/api/public/project_clkk1ucdyf6ak38svcatie9tf/subgraphs/nouns-builder-zora-mainnet/stable/gn',
+      'https://api.goldsky.com/api/public/project_cm33ek8kjx6pz010i2c3w8z25/subgraphs/nouns-builder-zora-mainnet/latest/gn',
+  [CHAIN_ID.OPTIMISM_SEPOLIA]:
+    null,
+  [CHAIN_ID.BASE_SEPOLIA]:
+    null,
   [CHAIN_ID.ZORA_SEPOLIA]:
-    'https://api.goldsky.com/api/public/project_clkk1ucdyf6ak38svcatie9tf/subgraphs/nouns-builder-zora-sepolia/stable/gn',
+    null,
   [CHAIN_ID.FOUNDRY]:
     'https://api.thegraph.com/subgraphs/name/neokry/nouns-builder-mainnet',
 }
