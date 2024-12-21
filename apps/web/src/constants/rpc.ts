ab423ce3305e6bed54aeee7f7caaf4adb1ce84b0
0xMillz <37815163+0xMillz@users.noreply.github.com>
2024-12-21 09:44:48 -0600

    Update turbo envs and fix build

--- apps/web/src/constants/rpc.ts
+++ apps/web/src/constants/rpc.ts
@@ -2,7 +2,7 @@ import { foundry } from 'wagmi/chains'
 
 import { CHAIN_ID } from 'src/typings'
 
-export const RPC_URL = {
+export const RPC_URL: Record<number, string> = {
   [CHAIN_ID.ETHEREUM]: `https://mainnet.gateway.tenderly.co/${process.env.NEXT_PUBLIC_TENDERLY_MAINNET_RPC_KEY}`,
   [CHAIN_ID.OPTIMISM]: `https://optimism.gateway.tenderly.co/${process.env.NEXT_PUBLIC_TENDERLY_OPTIMISM_RPC_KEY}`,
   [CHAIN_ID.SEPOLIA]: `https://sepolia.gateway.tenderly.co/${process.env.NEXT_PUBLIC_TENDERLY_SEPOLIA_RPC_KEY}`,
